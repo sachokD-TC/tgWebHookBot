a1be3d3aea4d19ecb393c79d93244315e4f4072e
sachokD-TC <ballynrallway@gmail.com>
2025-07-18 15:53:52 +0000

    keyboard fix

--- app.py
+++ app.py
@@ -9,19 +9,23 @@ TOKEN = os.environ.get("BOT_TOKEN")
 @app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
     print(f"inside webhook")
-    data = request.get_json()            
-    
-    keyboard = {
+    data = request.get_json()                
+    if "message" in data:        
+        chat_id = data["message"]["chat"]["id"]            
+        text = data["message"].get("text", "")
+        keyboard = {
         "inline_keyboard": [
             [{"text": "Say Hello", "callback_data": "say_hello"}],
             [{"text": "Show Time", "callback_data": "show_time"}]
         ]
-    }
-    requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", json={
+        }
+        requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", json={
         "chat_id": chat_id,
         "text": "Choose an action:",
         "reply_markup": keyboard
-    })
+        })
+    
+    
 
     if "callback_query" in data:
         chat_id = data["callback_query"]["message"]["chat"]["id"]
@@ -38,23 +42,7 @@ def webhook():
     requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", json={
         "chat_id": chat_id,
         "text": reply
-    })
-
-    if "message" in data:        
-        chat_id = data["message"]["chat"]["id"]            
-        text = data["message"].get("text", "")
-        keyboard = {
-            "inline_keyboard": [
-                [{"text": "Старт", "url": "https://example.com"}],
-                [{"text": "Хорошо", "callback_data": "Как прошел день"}]
-            ]
-        }
-    
-        if "/start" in text:
-            postReply(chat_id, "Доброе утро, Нина 🌞\nКак твои дела сегодня?\n\n"
-        "Я тут, рядом. Напиши мне, как ты себя чувствуешь. "
-        "А если хочешь — просто нажми на кнопку ниже:")                
-    return {"ok": True}
+    })        
 
 def postReply(chat_id, reply):
     requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", json={
