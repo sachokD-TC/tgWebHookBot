4a3b43a02f4315af0ac61a2efcbc7d2d00a3fd19
sachokD-TC <ballynrallway@gmail.com>
2025-07-18 11:30:03 +0000

    fix port

--- app.py
+++ app.py
@@ -22,5 +22,6 @@ def webhook():
 def index():
     return "Telegram bot is running!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__":   
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
