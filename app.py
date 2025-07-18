de5a306d5c1665fe0152d416e8bb30ef4278f170
sachokD-TC <ballynrallway@gmail.com>
2025-07-18 13:43:13 +0000

    add start

--- app.py
+++ app.py
@@ -9,17 +9,22 @@ TOKEN = os.environ.get("BOT_TOKEN")
 @app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
     print(f"inside webhook")
-    data = request.get_json()
-    print(f"data - {data}")
+    data = request.get_json()    
     if "message" in data:
-        chat_id = data["message"]["chat"]["id"]
+        chat_id = data["message"]["chat"]["id"]            
         text = data["message"].get("text", "")
-        reply = f"You said: {text}"
-        requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", json={
+        if "/start" in text:
+            postReply(chat_id, "Доброе утро, Нина 🌞\nКак твои дела сегодня?\n\n"
+        "Я тут, рядом. Напиши мне, как ты себя чувствуешь. "
+        "А если хочешь — просто нажми на кнопку ниже:")                
+    return {"ok": True}
+
+def postReply(chat_id, reply):
+    requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", json={
             "chat_id": chat_id,
             "text": reply
         })
-    return {"ok": True}
+
 
 @app.route("/")
 def index():
